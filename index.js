7ea05d9d007e5e2daa383e9452a991fca6490c5d
CHERYL-8 <ugwuezeobiomacharity@gmail.com>
2025-05-14 02:08:10 +0100

    Changed font Added axios

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function generateSong(event) {
-    event.preventDefault();
+function showSong(response) {
     new Typewriter("#song", {
-       strings: "song song",
+       strings: response.data.answer,
        autoStart: true,
        delay: 1,
        cursor: ""
-    });
+    });  
+}
+
+function generateSong(event) {
+    event.preventDefault();
+    let user = document.querySelector("#userInput");
+    let apiKey = "c8c71f823b69ab4b8b86o3a0f1d320t8";
+    let prompt = `Generate a song about the ${user.value}`;
+    let context = "you are a creative and an excellent song composer,generate a song that has a verse and a chorus,the lyrics of the song should be in a seperate line,make sure to follow the user instructions,sign 'Ugwueze Obioma AI' at the end of the song in italics using the color code #ff4d00";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    axios.get(apiUrl).then(showSong);
+
+
+
 }
 let inputForm = document.querySelector("#songForm");
 inputForm.addEventListener("submit",generateSong);
