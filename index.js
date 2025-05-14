18ccdbbe41ece5c0cc884e970688e617834e3ebf
CHERYL-8 <ugwuezeobiomacharity@gmail.com>
2025-05-14 03:18:16 +0100

    Refining context

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function generateSong(event) {
     let user = document.querySelector("#userInput");
     let apiKey = "c8c71f823b69ab4b8b86o3a0f1d320t8";
     let prompt = `Generate a song about the ${user.value}`;
-    let context = "you are a creative and an excellent song composer,generate a song that has a verse and a chorus,the lyrics of the song should be in a seperate line,make sure to follow the user instructions,sign 'Ugwueze Obioma AI' at the end of the song in italics using the color code #ff4d00";
+    let context = "you are a creative and an excellent song composer.generate a song in basic html.seperate using <br/>.the song should have a chorus.make sure to follow the user instructions.sign 'Ugwueze Obioma AI' at the end of the song in italics using the color code #ff4d00.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(showSong);
 }
