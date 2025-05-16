b890ceb8defadb4b9a95551774c843ef1282221b
CHERYL-8 <ugwuezeobiomacharity@gmail.com>
2025-05-16 03:11:57 +0100

    Still

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ function showSong(response) {
 
 function generateSong(event) {
     event.preventDefault();
-    let user = document.querySelector("#userInput");
+    let user = document.querySelector("#user-input");
     let apiKey = "c8c71f823b69ab4b8b86o3a0f1d320t8";
     let prompt = `user input:Generate a song about the ${user.value}`;
     let context = "you are a creative and an excellent song composer.generate a song in basic html.seperate using <br/>.the song should have a chorus.make sure to follow the user input.sign 'Ugwueze Obioma AI' at the end of the song in italics using the color code #ff4d00.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
     axios.get(apiURL).then(showSong);
 }
-let inputForm = document.querySelector("#songForm");
-inputForm.addEventListener("submit",generateSong);
+let formElement = document.querySelector("#song-form");
+formElement.addEventListener("submit",generateSong);
