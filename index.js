4b36a855d4fcac50fd2141b8113631d27e682f6d
CHERYL-8 <ugwuezeobiomacharity@gmail.com>
2025-05-20 01:57:45 +0100

    more work

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ function showSong(response) {
 
 function generate(event) {
   event.preventDefault();
-  let apiKey = "c8c71f823b69ab4b8b86o3a0f1d320t8";
   let context =
     "you are a creative and an excellent song composer. Generate a 10 line song in basic HTML. Separate using <br/>. Sign 'Ugwueze Obioma AI' at the end of the song in italics using the color code #ff4d00.";
   let user = document.querySelector("#user-input");
   let prompt = `User input: Generate a song about ${user.value}`;
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?context=${context}&prompt=${prompt}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?context=${context}&prompt=${prompt}&key=c8c71f823b69ab4b8b86o3a0f1d320t8`;
   let submitButton = document.querySelector('#command')
   submitButton.setAttribute('disabled', 'disabled');
 
